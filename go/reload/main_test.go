027cfb62c300f1ae67f3a2e36435a73dbf5b637b
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2024-11-09 17:36:58 +0100

    check for correct title

--- go/reload/main_test.go
+++ go/reload/main_test.go
@@ -9,26 +9,25 @@ import (
 	"time"
 )
 
-var urls = []string{
-	"https://notrelated.xyz/rss",
-	"https://www.archlinux.org/feeds/news/",
-	"https://www.pathofexile.com/news/rss",
-	"https://fractalsoftworks.com/feed/",
-	"https://odysee.com/$/rss/@ShortFatOtaku:1",
-	"https://www.youtube.com/feeds/videos.xml?user=CaravanPalace",
-}
-
-func TestGetUrlContent(t *testing.T) {
+func TestGetRss(t *testing.T) {
+	testFeeds := map[string]string{
+		"Not Related! A Big-Braned Podcast": "https://notrelated.xyz/rss",
+		"Arch Linux: Recent news updates":   "https://www.archlinux.org/feeds/news/",
+		"Path of Exile News":                "https://www.pathofexile.com/news/rss",
+		"Starsector":                        "https://fractalsoftworks.com/feed/",
+		"ShortFatOtaku on Odysee":           "https://odysee.com/$/rss/@ShortFatOtaku:1",
+		"CaravanPalace":                     "https://www.youtube.com/feeds/videos.xml?user=CaravanPalace",
+	}
 	cacheTime := 60 * time.Minute
 	cachePath := path.Join(os.Getenv("HOME"), ".cache", "nvimboat-test")
-	err := os.MkdirAll(cachePath, 755)
+	err := os.MkdirAll(cachePath, 0755)
 	if err != nil {
 		t.Fatal("cannot create cache directory")
 	}
 	header := http.Header{
 		"User-Agent": {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"},
 	}
-	for _, url := range urls {
+	for title, url := range testFeeds {
 		fmt.Println("first iteration...")
 		GetRss(url, header, cacheTime, cachePath)
 		fmt.Println("now try to get contents from cache...")
@@ -39,6 +38,8 @@ func TestGetUrlContent(t *testing.T) {
 		if !fromCache {
 			t.Fatal("did not read from cache for", url)
 		}
-		fmt.Println(feed.Title)
+		if title != feed.Title {
+			t.Errorf("expected: %s, parsed: %s\n", title, feed.Title)
+		}
 	}
 }
