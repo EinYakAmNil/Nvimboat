2c98277a16d8458186ff291c7037c2df20002d76
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2025-09-13 22:49:00 +0200

    reformat

--- go/main.go
+++ go/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-
 	"github.com/EinYakAmNil/Nvimboat/go/engine/nvimboat"
 	nvimPlugin "github.com/neovim/go-client/nvim/plugin"
 )
@@ -9,8 +8,18 @@ import (
 func main() {
 	nb := new(nvimboat.Nvimboat)
 	nvimPlugin.Main(func(p *nvimPlugin.Plugin) (err error) {
-		p.HandleCommand(&nvimPlugin.CommandOptions{Name: "Nvimboat", NArgs: "+", Complete: "customlist, CompleteNvimboat"}, nb.HandleAction)
-		p.HandleFunction(&nvimPlugin.FunctionOptions{Name: "CompleteNvimboat"}, nvimboat.CompleteNvimboat)
+		p.HandleCommand(
+			&nvimPlugin.CommandOptions{
+				Name:     "Nvimboat",
+				NArgs:    "+",
+				Complete: "customlist, CompleteNvimboat",
+			},
+			nb.HandleAction,
+		)
+		p.HandleFunction(
+			&nvimPlugin.FunctionOptions{Name: "CompleteNvimboat"},
+			nvimboat.CompleteNvimboat,
+		)
 		return
 	})
 }
