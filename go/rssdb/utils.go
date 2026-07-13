b74c7ec3c4ef0991cf867cb143bd46b7fd4953f0
EinYakAmNil <linkai.zhang@einyakamnil.xyz>
2026-07-13 22:33:06 +0200

    fix: create db directory if missing

--- go/rssdb/utils.go
+++ go/rssdb/utils.go
@@ -5,8 +5,10 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"path"
 
 	_ "embed"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -23,13 +25,24 @@ func ConnectDb(dbPath string) (dbh DbHandle, err error) {
 	dbh.Ctx = context.Background()
 	dbh.DB, err = sql.Open("sqlite3", dbPath)
 	if err != nil {
-		err = fmt.Errorf("ConnectDb: %w", err)
+		err = fmt.Errorf("sql.Open: %w\n"+
+			"rssdb/ConnectDb", err,
+		)
 		return
 	}
 	// only create tables, if the database does not exist yet
 	if _, noDbErr := os.Stat(dbPath); noDbErr != nil {
+		err = os.MkdirAll(path.Dir(dbPath), 0755)
+		if err != nil {
+			err = fmt.Errorf("os.MkdirAll: %w\n"+
+				"rssdb/ConnectDb", err,
+			)
+			return
+		}
 		if _, err = dbh.DB.ExecContext(dbh.Ctx, schemaSql); err != nil {
-			err = fmt.Errorf("ConnectDb: %w", err)
+			err = fmt.Errorf("dbh.DB.ExecContext: %w\n"+
+				"rssdb/ConnectDb", err,
+			)
 			return
 		}
 	}
